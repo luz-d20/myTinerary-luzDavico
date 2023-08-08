17e6727ec001780468beb8112c7ee27093ff4e91
Luz Davico <mldavico@gmail.com>
2023-08-08 18:47:22 -0300

    Fixed img routing + added slider & fake DB

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,23 +7,21 @@ import { useState, useEffect } from 'react';
 export default function Navigation() {
   /*Defino Navbar inicializándolo como Falso */
   const [navbar, setNavbar] = useState(false)
-  /*Sí scroll > 250px Navbar pasa a ser True*/
+  /*Sí scroll > 100px Navbar pasa a ser True*/
   const changeBackground = () => {
-    if (window.scrollY >= 250) {
+    if (window.scrollY >= 100) {
       setNavbar(true)
-      console.log("true")
     } else {
       setNavbar(false)
-      console.log("false")
     }
   }
   /*Ejecuto la función changeBackground cada vez que se produce un scroll*/
   window.addEventListener('scroll', changeBackground)
   /* Con un classname ternario: si es TRUE, bg de color sólido / si es FALSE, transparente */
   return (
-    <Navbar variant="dark" fixed="top" expand="lg" className={navbar ? "bg-light p-30" : "bg-transparent p-30"}>
+    <Navbar variant={navbar? "light" : "dark"} fixed="top" expand="lg" className={navbar ? "bg-light p-30" : "bg-transparent p-30"}>
         <Container fluid>
-          <Navbar.Brand className="flex-row nav-brand" href="#home"><img width="124" height="124" src="src/assets/myTinerary_logo.png" alt="logo" />
+          <Navbar.Brand className="flex-row nav-brand" href="#home"><img width="100" height="100" src="/myTinerary_logo.png" alt="logo" />
           <h1 className="green mobile-hide"><span>my</span>Tinerary</h1></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -31,7 +29,7 @@ export default function Navigation() {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Cities</Nav.Link>
             <Nav.Link href="#link">
-              <img width="30" height="30" src="src/assets/user_png.png" alt="user icon" />
+              <img width="30" height="30" src="/user_png.png" alt="user icon" />
               Log In
               </Nav.Link>
           </Nav>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,168 @@
 import React from 'react'
+import { Carousel } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { useState, useEffect } from 'react';
+
+const fakedb = 'fakeDB.json'
 
 export default function Slider() {
+  const [jsonData, setJsonData] = useState(null)
+
+  /* Fetch local JSON database or return an error */
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(fakedb);
+        const data = await response.json();
+        setJsonData(data);
+      } catch (error) {
+        console.error('Error fetching JSON:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  /* Return loading message while waiting for JSON data */
+  if (jsonData === null) {
+    return <div>Loading...</div>;
+  }
+  /* Render JSON data within the slider */
   return (
-    <div>
-      <h1>Slider</h1>
-    </div>
+    <Container className="sliderContainer">
+    <Carousel variant="dark">
+      <Carousel.Item interval={1500}>
+        <div className='gridParent'>
+          <div className='gridChild1'>
+            <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[0].img}
+              alt={jsonData.cities[0].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[0].name}, {jsonData.cities[0].country}
+            </p>
+          </div>
+          <div className='gridChild2'>
+            <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[1].img}
+              alt={jsonData.cities[1].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[1].name}, {jsonData.cities[1].country}
+            </p>
+          </div>
+          <div className='gridChild3'>
+            <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[2].img}
+              alt={jsonData.cities[2].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[2].name}, {jsonData.cities[2].country}
+            </p>
+          </div>
+          <div className='gridChild4'>
+            <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[3].img}
+              alt={jsonData.cities[3].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[3].name}, {jsonData.cities[3].country}
+            </p>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item interval={1500}>
+        <div className='gridParent'>
+          <div className='gridChild1'>
+            <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[4].img}
+              alt={jsonData.cities[4].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[4].name}, {jsonData.cities[4].country}
+            </p>
+          </div>  
+          <div className='gridChild2'>
+            <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[5].img}
+              alt={jsonData.cities[5].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[5].name}, {jsonData.cities[5].country}
+            </p>
+          </div>
+          <div className='gridChild3'>
+            <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[6].img}
+              alt={jsonData.cities[6].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[6].name}, {jsonData.cities[3].country}
+            </p>
+          </div>
+          <div className='gridChild4'>
+            <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[7].img}
+              alt={jsonData.cities[7].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[7].name}, {jsonData.cities[7].country}
+            </p>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item interval={1500}>
+        <div className='gridParent'>
+          <div className='gridChild1'>
+            <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[8].img}
+              alt={jsonData.cities[8].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[8].name}, {jsonData.cities[8].country}
+            </p>
+          </div>  
+          <div className='gridChild2'>
+          <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[9].img}
+              alt={jsonData.cities[9].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[9].name}, {jsonData.cities[9].country}
+            </p>
+          </div>
+          <div className='gridChild3'>
+          <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[10].img}
+              alt={jsonData.cities[10].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[10].name}, {jsonData.cities[10].country}
+            </p>
+          </div>
+          <div className='gridChild4'>
+          <img
+              className="d-block w-100 h-100"
+              src={jsonData.cities[11].img}
+              alt={jsonData.cities[11].name} width="250" height="250"
+            />
+            <p className="text-s carouselCaption">
+              {jsonData.cities[11].name}, {jsonData.cities[11].country}
+            </p>
+          </div>
+        </div>
+      </Carousel.Item>
+    </Carousel>
+    </Container>
   )
 }
